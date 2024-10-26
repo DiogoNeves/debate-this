f55710a0c0633d1de546d3a687af3fd746b76de6
Diogo Neves <diogo.neves@gmail.com>
2024-10-26 22:51:41 +0100

    adds pauses to more easily remove annotations in edit

--- generate_speech.py
+++ generate_speech.py
@@ -11,10 +11,10 @@ api_key = os.getenv("ELEVENLABS_API_KEY")
 client = ElevenLabs(api_key=api_key)
 
 
-text = '''"How dare you interrupt me while I'm working!" he shouted angrily.
-"This is absolutely ridiculous," he growled in frustration.
-"I can't believe I have to deal with this nonsense!"
-"ENOUGH!" he bellowed with rage.
+text = '''"How dare you interrupt me while I'm working!" - he shouted angrily.
+"This is absolutely ridiculous," -he growled in frustration.
+"I can't believe I have to deal with this nonsense!" - he exclaimed.
+"ENOUGH!" - he bellowed with rage.
 "I've had it with these constant disruptions!"'''
 
 
