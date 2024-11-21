61ef87b2f5fc7ca100c8a84e0e84c5176448ec47
Diogo Neves <diogo.neves@gmail.com>
2024-11-21 23:25:48 +0000

    extracts emotional speech

--- generate_speech_post_process.py
+++ generate_speech_post_process.py
@@ -2,6 +2,8 @@ import requests
 import os
 import json
 import base64
+import ffmpeg
+import tempfile
 from dataclasses import dataclass
 from dotenv import load_dotenv
 
@@ -17,6 +19,9 @@ class Alignments:
     characters: list[str]
     character_start_times_seconds: list[float]
     character_end_times_seconds: list[float]
+
+
+Range = tuple[int, int]
 
 
 def _get_headers():
@@ -67,8 +72,31 @@ def _get_ranges_to_keep(alignment: Alignments) -> list[tuple[int, int]]:
     return ranges
 
 
-def _save_audio(audio_bytes: bytes) -> None:
-    output_path = "output/post_process.mp3"
+def _get_speech_from_audio(audio_bytes: bytes,
+                           ranges_to_keep: list[Range]) -> bytes:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_input:
+        temp_input.write(audio_bytes)
+        temp_input_path = temp_input.name
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_output:
+        temp_output_path = temp_output.name
+
+    inputs = [ffmpeg.input(temp_input_path, ss=start, to=end)
+              for start, end in ranges_to_keep]
+    joined = ffmpeg.concat(*inputs, v=0, a=1).output(temp_output_path)
+    ffmpeg.run(joined)
+
+    with open(temp_output_path, "rb") as f:
+        result_audio_bytes = f.read()
+
+    os.remove(temp_input_path)
+    os.remove(temp_output_path)
+
+    return result_audio_bytes
+
+
+def _save_audio(audio_bytes: bytes, file_name: str) -> None:
+    output_path = f"output/{file_name}.mp3"
     with open(output_path, "wb") as f:
         f.write(audio_bytes)
 
@@ -101,12 +129,17 @@ def generate_speech():
     )
     response.raise_for_status()
 
-    audio_bytes, alignment = _parse_response(response)
+    raw_audio_bytes, alignment = _parse_response(response)
+    
+    # We have to extract the relevant speech from the audio
     ranges_to_keep = _get_ranges_to_keep(alignment)
+    real_speech_audio_bytes = _get_speech_from_audio(raw_audio_bytes,
+                                                     ranges_to_keep)
 
-    print("Ranges to keep:", ranges_to_keep)
-    _save_audio(audio_bytes)
-    print(alignment)
+    _save_audio(raw_audio_bytes, "raw")
+    _save_audio(real_speech_audio_bytes, "post_process")
+
+    print("Post-process completed successfully.")
 
 
 if __name__ == "__main__":
